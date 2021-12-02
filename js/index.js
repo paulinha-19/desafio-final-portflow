d3782d9a006572e48b5bc2a5d4eccf9d486bd3a8
Paulinha <paulafabianasoares@gmail.com>
2021-12-02 20:31:54 -0300

    menu hamburguer não finalizado

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ $('li a').click(function(e) {
     $('li a.active').removeClass('active');
     $(this).addClass('active');
 });
+
+function openFunction(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+    document.getElementById("mainbox").innerHTML="";
+   }
+  function closeFunction(){
+   document.getElementById("menu").style.width="0px";
+   document.getElementById("mainbox").style.marginLeft="0px";
+   document.getElementById("mainbox").innerHTML="&#9776;";
+  }
