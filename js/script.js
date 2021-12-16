4ed1cfbc33444481cda315472bb95b541ee690e1
Paulinha <paulafabianasoares@gmail.com>
2021-12-16 17:11:39 -0300

    menu hamburguer sem js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-$('li a').click(function(e) {
-    e.preventDefault();
-    $('li a.active').removeClass('active');
-    $(this).addClass('active');
-});
-
 const openNav = () => {
     document.getElementById("mySidenav").style.width = "50%";
 }
   
 const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
-  }
+}
